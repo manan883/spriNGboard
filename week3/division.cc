ac9907011838b5f5d1a7645583db99befa1e8b57
manan <manan@aws.com>
2023-02-08 21:30:39 -0800

    added integer division

---
+++ week3/division.cc
@@ -0,0 +1,49 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  //
+  // Output the header lines.
+  //
+
+  cout << setw(5) << "";
+  for (int32_t denom=1; denom<=10; ++denom) {
+    cout << setw(4) << showpos << denom;
+  }
+  cout << endl;
+
+  cout << setfill('-') << setw(5) << "";
+  for (int32_t denom=1; denom<=10; ++denom) {
+    cout << setfill('-') << setw(4) << "";
+  }
+  cout << endl;
+  cout << setfill(' '); // need to reset the fill character
+
+  //
+  // Fill in the table rows.
+  //
+
+  for (int32_t numer=-10; numer<=10; ++numer)
+  {
+    cout << setw(3) << showpos << numer << ": ";
+    for (int32_t denom=-10; denom<=10; ++denom)
+    {
+      try{
+      if (denom == 0){
+        throw denom;
+      }
+
+      int32_t result = numer/denom;
+      cout << setw(4) << result;
+      }
+      catch (int denom) {
+        cout << setw(4) << "E";
+      }
+    }
+    cout << endl;
+  }
+}
+
