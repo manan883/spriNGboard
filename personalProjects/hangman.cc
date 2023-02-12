eb378a440a4012182a1391d693753f3c521d1d4f
manan <manan@aws.com>
2023-02-11 16:19:20 -0800

    added personal projects

---
+++ personalProjects/hangman.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+string inputUser = "";
+void startScreen(){
+  //Print options to user
+  cout << "Welcome to CPP Hangman!" <<endl << "Play Options:" << endl
+  << "A: 2 player hangman" << endl << "B: Player vs CPU" << endl << "E: exit"<< endl;
+
+
+}
+
+
+
+
+
+
+
+
+int main ()
+{
+  startScreen();
+  return 0;
+}
