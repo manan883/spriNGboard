53925f629932dfb2b7f2344668b56a457de7e671
manan <manan@aws.com>
2023-02-18 19:51:01 -0800

    added algorithms

--- week4/algorithms.cc
+++ week4/algorithms.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-
+#include <numeric>
+#include <algorithm>
 ////////////////////////////////////
 // INCLUDE NECESSARY HEADERS HERE //
 ////////////////////////////////////
@@ -12,6 +13,10 @@ void print_vector (const vector<int>& v);
 
 void test_algorithms (int N)
 {
+  vector<int> v;
+  for (int i = 0; i <= N; ++i){
+      v.push_back(i);
+  }
   //////////////////////////////
   // CREATE A VECTOR v OF int //
   // WITH SIZE N              //
@@ -25,7 +30,7 @@ void test_algorithms (int N)
   // USE iota TO FILL v        //
   // WITH VALUES STARTING AT 5 //
   ///////////////////////////////
-
+  iota(v.begin(), v.end() + N, 5);
 
   print_vector(v);
 
@@ -35,14 +40,14 @@ void test_algorithms (int N)
   // LAST POSITIONS                    //
   ///////////////////////////////////////
 
-
+  reverse(v.begin() + 2, v.end() - (N-3));
   print_vector(v);
 
   /////////////////////////////////////
   // USE fill TO FILL THE FIRST FOUR //
   // VALUES WITH 20s                 //
   /////////////////////////////////////
-
+  fill(v.begin(), v.end() - (N-3),20);
 
   print_vector(v);
 
@@ -50,7 +55,7 @@ void test_algorithms (int N)
   // USE sort TO SORT ALL BUT THE FIRST //
   // AND LAST VALUES                    //
   ////////////////////////////////////////
-
+  sort(v.begin() + 1, v.end() -1);
 
   print_vector(v);
 
@@ -59,7 +64,13 @@ void test_algorithms (int N)
   // TO SORT ALL EVEN NUMBERS TO THE       //
   // FRONT AND ALL ODD NUMBERS TO THE BACK //
   ///////////////////////////////////////////
-
+/*
+sort(v.begin(), v.end(), [](int a)
+  {
+    return (a%2);
+  }
+);
+*/
 
   print_vector(v);
 }
